850090bd3700e56f7a955edd3be4f1b85e295727
Shivam Bansal <shivam_bansal2006@yahoo.com>
2013-06-18 14:58:45 +0530

    some refractoring

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -7,9 +7,9 @@ class StationsController < ApplicationController
 		if @station.update_attributes(nameStation: params[:nameStation], territory_id: params[:territory_id])
 	      flash[:success] = "Territory updated"
 	      redirect_to stations_path
-	    else
+	  else
 	      render 'edit'
-	    end
+	  end
 	end
 
 	def edit
@@ -21,43 +21,23 @@ class StationsController < ApplicationController
 	end
 
 	def index
-		if current_user[:level_type]!= nil 
-			if current_user.admin?
-				@stations = Station.all
-				@name = "Central Team" 
-			elsif Region.find(current_user.level_id).nameRegion=="India"
-				@stations=Station.where("nameStation NOT LIKE 'ATS-WTC'")
-				@name = "India"
-			else
-				@stations=[]
-				if current_user.level_type == "Region" 
-					@region = Region.find(current_user.level_id)
-					@territories = @region.territories.all
-					@territories.each do |t|
-						@stations =@stations + t.stations
-					end
-					@name = @region.nameRegion
-				elsif current_user[:level_type] == "Territory"
-					@stations = Territory.find(current_user.level_id).stations
-					@name=Territory.find(current_user.level_id).nameTerritory
-				else
-					@stations = Station.find(current_user.level_id)
-					@name = Station.find(current_user).nameStation
-				end
-			end
-		else
+		result = user_access_stations(current_user)
+		if result == nil
 			flash.now[:notice] = "No station assigned to you"
+		else
+			@stations = result[:stations]
+			@name =	result[:name]
 		end
 	end
 
 	def create
-	  	@station = Station.new(nameStation: params[:nameStation], territory_id: params[:territory_id])
-	  	if @station.save
-	      flash[:success] = "Successfully added #{@station.nameStation}"
-	      redirect_to stations_path
-	    else
-	    	render 'new'
-	  	end
-  	end
+	  @station = Station.new(nameStation: params[:nameStation], territory_id: params[:territory_id])
+	  if @station.save
+	    flash[:success] = "Successfully added #{@station.nameStation}"
+	    redirect_to stations_path
+	  else
+	    render 'new'
+	  end
+  end
 
 end
--- app/helpers/stations_helper.rb
+++ app/helpers/stations_helper.rb
@@ -1,4 +1,34 @@
 module StationsHelper
 
+	def user_access_stations(user)
+		if user[:level_type]!= nil 
+			if user.admin?
+				@stations = Station.all
+				@name = "Central Team" 
+			elsif Region.find(user.level_id).nameRegion=="India"
+				@stations=Station.where("nameStation NOT LIKE 'ATS-WTC'")
+				@name = "India"
+			else
+				@stations=[]
+				if user.level_type == "Region" 
+					@region = Region.find(user.level_id)
+					@territories = @region.territories.all
+					@territories.each do |t|
+						@stations =@stations + t.stations
+					end
+					@name = @region.nameRegion
+				elsif user[:level_type] == "Territory"
+					@stations = Territory.find(user.level_id).stations
+					@name=Territory.find(user.level_id).nameTerritory
+				else
+					@stations = Station.find(user.level_id)
+					@name = Station.find(user).nameStation
+				end
+			end
+			{stations: @stations, name: @name}
+		else
+			nil
+		end		
+	end
 	
 end
